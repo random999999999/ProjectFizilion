c4b650b94e4d75e32fbfe5d12c048e1a4e6b88cf
random999999999 <58245161+random999999999@users.noreply.github.com>
2022-02-18 16:42:03 -0400

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -144,7 +144,7 @@ ANTI_SPAMBOT_SHOUT = sb(os.environ.get("ANTI_SPAMBOT_SHOUT") or "False")
 ALIVE_NAME = os.environ.get("ALIVE_NAME") or None
 
 # Default .alive logo
-ALIVE_LOGO = str(os.environ.get("ALIVE_LOGO") or "https://c.tenor.com/AQecc2g8uuAAAAAC/duck-dance.gif")
+ALIVE_LOGO = str(os.environ.get("ALIVE_LOGO") or "https://github.com/FrosT2k5/ProjectFizilion/raw/dragon/resources/IMG_20201109_130207_262.jpg")
 
 # Custom Alive Message
 ALIVE_MESSAGE = str(os.environ.get("ALIVE_MESSAGE") or "")
